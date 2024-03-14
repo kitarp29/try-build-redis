3e72453bb0b80ad7f40571ada87893f3ff8409e2
kitarp29 <kitarpsingrajpoot@gmail.com>
2024-03-14 20:38:58 +0530

    I still think I could do better
    Signed-off-by: kitarp29 <kitarpsingrajpoot@gmail.com>

--- app/server.go
+++ app/server.go
@@ -2,38 +2,43 @@ package main
 
 import (
 	"fmt"
-	//Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	data := []byte("+PONG\r\n")
-	_, err = conn.Write(data)
-	if err != nil {
-		fmt.Println("Error writing data to connection: ", err.Error())
-		os.Exit(1)
+	count := 0
+	for {
+		data := make([]byte, 1024)
+		_, err := conn.Read(data)
+		if err != nil {
+			fmt.Println("Error reading data from connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		count++
+		response := "+PONG\r\n"
+		_, err = conn.Write([]byte(response))
+		if err != nil {
+			fmt.Println("Error writing data to connection: ", err.Error())
+			os.Exit(1)
+		}
 	}
+
 	conn.Close()
+
 }
