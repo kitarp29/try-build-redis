353b70913f32d29a38bc1f730e82fe98d1f35470
kitarp29 <kitarpsingrajpoot@gmail.com>
2024-03-15 00:39:14 +0530

    fucking finally

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -31,16 +32,30 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	data := make([]byte, 1024)
 	for {
-		_, err := conn.Read(data)
+		s, err := conn.Read(data)
 		if err != nil {
 			fmt.Println("Error reading data from connection: ", err.Error())
 			os.Exit(1)
 		}
-		response := "+PONG\r\n"
-		_, err = conn.Write([]byte(response))
-		if err != nil {
-			fmt.Println("Error writing data to connection: ", err.Error())
-			os.Exit(1)
+		str := (string(data[:s]))
+		if strings.Contains(str, "ping") {
+			response := "+PONG\r\n"
+			_, err = conn.Write([]byte(response))
+			if err != nil {
+				fmt.Println("Error writing data to connection: ", err.Error())
+				os.Exit(1)
+			}
+		} else {
+			split := strings.Split(str, "\r\n")
+			// fmt.Println(split, len(split))
+			// fmt.Printl=n(split[len(split)-2])
+			response := "+" + split[len(split)-2] + "\r\n"
+			// fmt.Println(response)
+			_, err = conn.Write([]byte(response))
+			if err != nil {
+				fmt.Println("Error writing data to connection: ", err.Error())
+				os.Exit(1)
+			}
 		}
 	}
 }
